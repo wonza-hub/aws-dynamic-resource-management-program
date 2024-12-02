1fce654dee866825a52f183ed9b7201c1f77a2ce
wonza-hub <wonza8585@gmail.com>
2024-12-02 14:28:20 +0900

    feat: EC2 클라이언트 초기 구성 설정하는 객체 추가

---
+++ src/config/ec2-client-config.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+// CONFIG: EC2 클라이언트 객체
+export const ec2ClientConfig = {
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
