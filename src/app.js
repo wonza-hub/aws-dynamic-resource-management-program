89a8558a9e3ac8725c31375fee67c67464e6a0fd
wonza-hub <wonza8585@gmail.com>
2024-12-03 20:36:10 +0900

    chore: app에 뷰 관련 설정 추가

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // JSON 데이터 파싱
 
+// 뷰 설정
+app.set("view engine", "ejs");
+app.set("views", `${process.cwd()}/views`);
+
+// 정적 리소스 경로 설정
+app.use(express.static(`${process.cwd()}/public`));
 //라우터 설정
 app.use("/ec2", ec2Router);
 
