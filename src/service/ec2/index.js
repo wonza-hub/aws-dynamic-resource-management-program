4b01523bca9f12cc4c936018b18675b519d58ecd
wonza-hub <wonza8585@gmail.com>
2024-12-07 16:37:45 +0900

    feat: ec2 객체에 관련 서비스 추가

--- src/service/ec2/index.js
+++ src/service/ec2/index.js
@@ -1,6 +1,8 @@
 // 여러 개의 서비스 함수들을 ec2Service 객체로 통합
 import controlInstances from "./control-instances.service.js";
+import createASG from "./create-asg.service.js";
 import createInstances from "./create-instances.service.js";
+import getCondorQueueStatus from "./get-condor-queue-status.service.js";
 import getCondorStatus from "./get-condor-status.service.js";
 import listAvailabilityRegions from "./list-availability-regions.service.js";
 import listAvailabilityZones from "./list-availability-zones.service.js";
@@ -9,6 +11,7 @@ import listInstances from "./list-instances.service.js";
 import rebootInstances from "./reboot-instances.service.js";
 import startInstances from "./start-instances.service.js";
 import stopInstances from "./stop-instance.service.js";
+import submitCondorJob from "./submit-condor-job.service.js";
 
 const ec2Service = {
   createInstances,
@@ -21,6 +24,10 @@ const ec2Service = {
   stopInstances,
   getCondorStatus,
   controlInstances,
+  createASG,
+  getCondorQueueStatus,
+  submitCondorJob,
+  getCondorQueueStatus,
 };
 
 export default ec2Service;
