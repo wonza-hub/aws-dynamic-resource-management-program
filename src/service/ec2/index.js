e36d620afabdaade4dea9b1ec672908150cec013
wonza-hub <wonza8585@gmail.com>
2024-12-08 22:56:04 +0900

    feat: 가용 리전 뷰 수정

--- src/service/ec2/index.js
+++ src/service/ec2/index.js
@@ -5,7 +5,7 @@ import createInstances from "./create-instances.service.js";
 import getCondorQueueStatus from "./get-condor-queue-status.service.js";
 import getCondorStatus from "./get-condor-status.service.js";
 import getHTCondorMetrics from "./get-htcondor-metrics.service.js";
-import listAvailabilityRegions from "./list-availability-regions.service.js";
+import listRegions from "./list-availability-regions.service.js";
 import listAvailabilityZones from "./list-availability-zones.service.js";
 import listImages from "./list-images.service.js";
 import listInstances from "./list-instances.service.js";
@@ -13,7 +13,7 @@ import submitCondorJob from "./submit-condor-job.service.js";
 
 const ec2Service = {
   createInstances,
-  listAvailabilityRegions,
+  listRegions,
   listAvailabilityZones,
   listImages,
   listInstances,
